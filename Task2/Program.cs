1924ea494b68bdf6675a90181f6e6954ecaff889
Maxim <maxmaxmaxmaxmaxa@mail.ru>
2022-11-22 16:20:44 +0300

    Сделали задание 2

--- Task2/Program.cs
+++ Task2/Program.cs
@@ -1,2 +1,29 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.WriteLine("Введите число A");
+int A = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите число B");
+int B = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите число C");
+int C = Convert.ToInt32(Console.ReadLine());
+int max = A;
+if (A > max)
+{
+    Console.WriteLine(max = A);
+}
+else
+{
+    if (B > max)
+      {
+        Console.WriteLine(max = B);
+      }
+      else 
+      {
+        if (C > max)
+         {
+          Console.WriteLine(max = C);
+         }
+         else 
+         {
+            Console.WriteLine(max);
+           }
+      }
+}
