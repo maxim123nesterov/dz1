b15a0443c8d2f019576e4cb6e95b7ff824ebf1f5
Maxim <maxmaxmaxmaxmaxa@mail.ru>
2022-11-22 16:50:09 +0300

    Выполнили задание 3

---
+++ task3/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.WriteLine("Веедите число");
+int A = Convert.ToInt32(Console.ReadLine());
+int B = 2;
+if (A % 2 == 0)
+{
+    Console.WriteLine("Да");
+}
+else
+{
+    Console.WriteLine("Нет");
+}
