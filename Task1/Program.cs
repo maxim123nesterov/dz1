d66673ad0a8abb94d7be71f777fc36cd6f19d0b2
Maxim <maxmaxmaxmaxmaxa@mail.ru>
2022-11-22 16:09:12 +0300

    сделали задание 1

---
+++ Task1/Program.cs
@@ -0,0 +1,22 @@
+﻿Console.WriteLine("Введите число A");
+int A = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите число B");
+int B = Convert.ToInt32(Console.ReadLine());
+int max = A;
+if (A > max)
+{
+    Console.WriteLine(max = A);
+}
+else
+{
+    if (B > max)
+      {
+        Console.WriteLine(max = B);
+      }
+      else 
+      {
+        Console.WriteLine(max);
+      }
+}    
+    
+      
